34a724783c307e02f032fecd004ad146f21d8a91
Blaine Hatab <jbhatb@gmail.com>
2025-01-05 15:37:11 -0500

    adding scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
           model: "gpt-3.5-turbo",
           messages: [{
             role: "user",
-            content: `Write me a fantastical biography for a dnd 5e character. Keep it to 500 words. Write it in the style of dnd writing. Here is the prompt ${inputText}`
+            content: `Write me a fantastical biography for a dnd 5e character. Keep it to 200 words. Write it in the style of dnd writing. Here is the prompt ${inputText}`
           }]
         })
       })
@@ -74,42 +74,47 @@ function App() {
 
   return (
     <>
-      <div className='grid grid-cols-3'>
-        <div></div>
-        <form onSubmit={handleSubmit} className='place-items-center'>
-          <input 
-            type="text"
-            value={inputText}
-            onChange={handleInputChange}
-            className="main-input"
-            placeholder="Enter your text"
-          />
-          <button 
-            type="submit"
-            className="btn btn-white"
-            disabled={loading}
-          >
-            {loading ? 'Loading...' : 'Submit'}
-          </button>
-        </form>
-        <div></div>
-      </div>
-      <div className='grid grid-cols-2'>
-        <div className='image'>
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-10 pb-4'>
+          <div></div>
+          <form onSubmit={handleSubmit} className='col-span-8 place-items-center flex'>
+            <input 
+              type="text"
+              value={inputText}
+              onChange={handleInputChange}
+              className="w-full px-4 py-2 text-gray-700 bg-white border rounded-lg focus:outline-none focus:border-blue-500 flex mr-4"
+              placeholder="Enter your text"
+            />
+            <button 
+              type="submit"
+              className="btn btn-white flex"
+              disabled={loading}
+            >
+              {loading ? 'Loading...' : 'Submit'}
+            </button>
+          </form>
+          <div></div>
         </div>
-        {bio && (
-          <div className='bio bg-white'>
-            {bio}
-          </div>
+        <div className="p-8 shadow-lg rounded-lg bg-[#E0E8DE]">
+          <div className='grid grid-cols-2'>
+            <div className='image'>
+              <img src="/gnome.png" alt="Character" className="w-full h-auto rounded" />
+            </div>
+            {bio && (
+              <div className='bio bg-white'>
+                {bio}
+              </div>
 
-        )}
-      </div>
-      <div className='grid'>
-        {story && (
-          <div className='story bg-white'>
-            {story}
+            )}
           </div>
-        )}
+          <div className='grid'>
+            {story && (
+              <div className='story bg-white'>
+                {story}
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </>
   )
